fd682877ebf537145057882cb93b0c357e1ef447
Rami Alhussien <Ramialhussein98@gmail.com>
2020-03-19 19:51:03 -0400

    adding two more questions 1.4 & 1.5

--- src/Chapter_1/Chapter_1_Questions.java
+++ src/Chapter_1/Chapter_1_Questions.java
@@ -19,8 +19,20 @@ public class Chapter_1_Questions {
         String w3 = "Mr John Smith";
         System.out.println(URLify(w3) + ' ' + w3.length());
 
+        System.out.println("---------------------------");
+        System.out.println("Palindrome Permutation: ");
+        String w4 = "tactcoa";
+        System.out.println(PalindromePermutation(w4));
+
+        System.out.println("---------------------------");
+        System.out.println("One Way: ");
+        String w5 = "pale";
+        String w6 = "bake";
+        System.out.println(OneWay(w5, w6));
+
     }
 
+    // 1.1
     static boolean isUnique(String w) {
         boolean Unique = true;
         for(int i = 1; i < w.length(); i++){
@@ -32,6 +44,7 @@ public class Chapter_1_Questions {
         return Unique;
     }
 
+    // 1.2
     static boolean isPermutation(String str1, String str2){
         boolean Permutation = true;
         char[] arr1 = str1.toCharArray();
@@ -46,6 +59,7 @@ public class Chapter_1_Questions {
         return Permutation;
     }
 
+    // 1.3
     static String URLify(String w){
         StringBuilder sentence = new StringBuilder();
 
@@ -60,4 +74,81 @@ public class Chapter_1_Questions {
         return sentence.toString();
     }
 
+    //1.4
+    static boolean PalindromePermutation(String w){
+         HashMap<Character, Integer> hash = new HashMap<>();
+
+         for(int i = 0; i < w.length(); i++ ){
+             hash.put(w.charAt(i), hash.getOrDefault(w.charAt(i), 0) + 1);
+         }
+         int count = 0;
+         for(char k: hash.keySet()){
+             count += hash.get(k) % 2;
+         }
+
+         if(count <= 1){
+             return true;
+         }else{
+             return false;
+         }
+    }
+
+    // 1.5
+    static boolean OneWay(String w1, String w2){
+        int v1 = w1.length();
+        int v2 = w2.length();
+
+        if(Math.abs(v1 - v2) > 1){
+            return false;
+        }
+
+//        HashMap<Character, Character> v1hashMap = new HashMap<>();
+//        for(int i = 0; i < w1.length(); i++){
+//            v1hashMap.put(w1.charAt(i), w1.charAt(i));
+//        }
+//
+//        HashMap<Character, Character> v2hashMap = new HashMap<>();
+//        for(int i = 0; i < w2.length(); i++){
+//            v2hashMap.put(w2.charAt(i), w2.charAt(i));
+//        }
+//
+//        for(char k: v1hashMap.keySet()){
+//            for (char c: v2hashMap.keySet()){
+//                if(v1hashMap.get(k) != v2hashMap.get(c)){
+//                    return false;
+//                }
+//            }
+//        }
+
+        int editCount = 0;
+        int i = 0;
+        int j = 0;
+        while(i < v1 && j < v2){
+            if(w1.charAt(i) != w2.charAt(j)){
+                if(editCount == 1){
+                    return false;
+                }
+
+                if(v1 > v2){
+                    i++;
+                } else if (v1 < v2) {
+                    j++;
+                } else {
+                    i++;
+                    j++;
+                }
+
+                editCount++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+
+        if(i < v1 || j < v2){
+            editCount++;
+        }
+
+        return true;
+    }
 }
